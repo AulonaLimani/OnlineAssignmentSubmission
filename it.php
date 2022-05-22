6798256ac4ddaf1734719ab046e313267d17fa7e
Art Ukshini <94797611+artukshini@users.noreply.github.com>
2022-05-23 00:31:39 +0200

    Update it.php

--- it.php
+++ it.php
@@ -10,3 +10,81 @@
   <link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
   <link href="http://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
 <style>
+  div.img {
+    border: 1px solid #ccc;
+}
+
+div.img:hover {
+    border: 1px solid #777;
+}
+
+div.img img {
+    width: 100%;
+    height: auto;
+}
+
+div.desc {
+    padding: 15px;
+    text-align: center;
+}
+
+* {
+    box-sizing: border-box;
+}
+
+.responsive {
+    padding: 0 16px;
+    float: left;
+    width: 24.99999%;
+}
+
+@media only screen and (max-width: 700px){
+    .responsive {
+        width: 49.99999%;
+        margin: 6px 0;
+    }
+}
+
+@media only screen and (max-width: 500px){
+    .responsive {
+        width: 100%;
+    }
+}
+
+.jumbotron {  
+    background-color:white;
+    color:#C94B4B;
+    padding:50px 15px;
+  }
+.jumbotron {
+    font-family: Montserrat, sans-serif;
+}
+.navbar {
+    margin-bottom: 0;
+    background-color: transparent;
+    z-index: 9999;
+    border: 0;
+    font-size: 12px !important;
+    line-height: 1.42857143 !important;
+    letter-spacing: 4px;
+    border-radius: 0;
+}
+
+.navbar li a, .navbar .navbar-brand {
+    color: #3C6E71 !important;
+    font-weight: bolder;}
+
+.navbar-nav li a:hover, .navbar-nav li.active a {
+    color: #fff !important;
+    background-color: #284B63 !important;
+}
+
+.navbar-default .navbar-toggle {
+    border-color: transparent;
+    color: #C94B4B !important;
+}
+.navbar {
+    font-family: Montserrat, sans-serif;
+}
+</style>
+</head>
