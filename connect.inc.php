ceb4ba4af75c261bc9caa415b29885d6b060196f
Aulona Limani <94397920+AulonaLimani@users.noreply.github.com>
2022-05-22 23:55:31 +0200

    Create connect.inc.php

---
+++ connect.inc.php
@@ -0,0 +1,9 @@
+<?php
+	$conn_error = 'Could not connect.';
+	$mysql_host = 'localhost';
+	$mysql_user = 'root';
+	$mysql_password = 'root';
+
+	$mysql_db = 'pi2';
+	$handle = @mysqli_connect('localhost', 'root', '','');
+?>
