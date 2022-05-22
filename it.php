1c27ee733f0971e04c79c7b050bf32701c6fe36a
Art Ukshini <94797611+artukshini@users.noreply.github.com>
2022-05-23 00:32:07 +0200

    Update it.php

--- it.php
+++ it.php
@@ -88,3 +88,82 @@ div.desc {
 }
 </style>
 </head>
+  <body>
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="" style="font-size:20px; color:#C94B4B";><span class="glyphicon glyphicon-pencil" style="color:#000"></span>BRIGHT MIND!</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+      <li><a href="loginfac.php"> HOME</a></li>
+   <li><a href="logoutfac.php"><span class="glyphicon glyphicon-log-out"></span> LOGOUT</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="jumbotron text-center">
+  <h1 id="h01" style="color: #3C6E71">IT FACULTIES</h1>
+</div>
+<div class="container">
+<?php
+    require 'connect.inc.php';
+    $query="SELECT * from Faculty where Department='IT'";
+$handle = @mysqli_connect('localhost', 'root', 'root','pi2');
+
+$query_run=mysqli_query($handle,$query);
+    $query_num_rows=mysqli_num_rows($query_run);
+    if($query_num_rows==0)
+    {
+        echo 'Sorry no faculties registered yet.';
+    }
+    else
+    {
+        while($query_row=mysqli_fetch_array($query_run))
+        {
+            $gender=$query_row['Gender'];
+            $first_name=$query_row['Firstname'];
+            $last_name=$query_row['Lastname'];
+            $contact=$query_row['Contact'];
+            $email_id=$query_row['Email_id'];
+            if($gender=='Male')
+            {
+?>
+<div class="responsive">
+  <div class="img">
+      <img src="images/img_avatar.png" alt="<?php echo $first_name.' '.$last_name; ?>" width="300" height="200">
+    <div class="desc">
+    <h4 style='color:green;'><?php echo $first_name.' '.$last_name; ?></h4>
+    <p><strong>Contact:</strong><?php echo $contact; ?></p>
+    <p><strong>Email id:</strong><?php echo $email_id; ?></p>
+    </div>
+  </div> 
+</div>
+<?php
+            }
+            else
+            {
+?>
+<div class="responsive">
+  <div class="img">
+      <img src="images/img_avatar2.png" alt="<?php echo $first_name.' '.$last_name; ?>" width="300" height="200">
+    <div class="desc">
+    <h4 style='color:green;'><?php echo $first_name.' '.$last_name; ?></h4>
+    <p><strong>Contact:</strong><?php echo $contact; ?></p>
+    <p><strong>Email id:</strong><?php echo $email_id; ?></p>
+    </div>
+  </div> 
+</div>
+<?php
+            }
+  }
+}
+?>
+</div>
+</body>
+</html>
