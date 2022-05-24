5e00fb4240733a881c89e7ab6279c1d8deebd195
Aulona Limani <94397920+AulonaLimani@users.noreply.github.com>
2022-05-24 22:58:45 +0200

    Update core.inc.php

--- core.inc.php
+++ core.inc.php
@@ -1,1 +1,21 @@
-
+<?php
+	ob_start();
+	session_start();
+	require 'connect.inc.php';
+	$current_file=$_SERVER['SCRIPT_NAME'];
+	if(isset($_SERVER['HTTP_REFERER']) && !empty($_SERVER['HTTP_REFERER']))
+	{
+		$http_referer=$_SERVER['HTTP_REFERER'];
+	}
+	function logged_in()
+	{
+        if(isset($_COOKIE['logged_in_student']))
+		{
+            $_SESSION['user_id_student'] = $_COOKIE['logged_in_student'];
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
