84165b6688135e9ada20a77e7d35406226936c28
Art Ukshini <94797611+artukshini@users.noreply.github.com>
2022-05-25 21:09:22 +0200

    Update loginform.inc.php

--- loginform.inc.php
+++ loginform.inc.php
@@ -25,3 +25,46 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
         echo '<script type="text/javascript">alert("Username or password not filled properly.")</script>';
     }
 }
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Student Login Form</title>
+
+
+    <link rel="stylesheet" href="styles/css/reset.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel='stylesheet prefetch' href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900'>
+    <link rel='stylesheet prefetch' href='http://fonts.googleapis.com/css?family=Montserrat:400,700'>
+    <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+
+    <link rel="stylesheet" href="styles/css/style.css">
+
+
+</head>
+
+<body style="background:url(images/Registration-photo.jpg)" ;>
+
+
+<div class="container">
+    <div class="info">
+        <h1>Login As Student</h1><span>
+    </div>
+</div>
+<div class="form">
+    <div class="thumbnail"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/hat.svg"/></div>
+    <form class="login-form" method="POST">
+        <p>Username:</p><br>
+        <input type="text" placeholder="username" name="username">
+        <p>Password:</p><br>
+        <input type="password" placeholder="password" name="password">
+        <button class="button" style="background-color: #3C6E71">Login</button>
+        <p class="message">Not registered? <a href="registration.php" style="color: black">Create an account</a></p>
+    </form>
+</div>
+
+</body>
+</html>
