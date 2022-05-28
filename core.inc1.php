008f1e90b68b740df8ea42c8638e9c574cbd6a9a
Aulona Limani <94397920+AulonaLimani@users.noreply.github.com>
2022-05-28 16:54:39 +0200

    Update core.inc1.php

--- core.inc1.php
+++ core.inc1.php
@@ -1,1 +1,31 @@
+<?php
+    ob_start();
+    @session_start();
+    require 'connect.inc.php';
+    $current_file = $_SERVER['SCRIPT_NAME'];
+    if (isset($_SERVER['HTTP_REFERER']) && !empty($_SERVER['HTTP_REFERER'])) {
+        $http_referer = $_SERVER['HTTP_REFERER'];
+    }
+    function logged_in1()
+    {
+        if (isset($_COOKIE['logged_in_fac'])) {
+            $_SESSION['user_id_fac'] = $_COOKIE['logged_in_fac'];
+            return true;
+        } else {
+            return false;
+        }
+    }
 
+    function getuserfields($field)
+    {
+        require 'connect.inc.php';
+        $query = "SELECT $field from Faculty where id='" . $_SESSION['user_id_fac'] . "'";
+        $handle = @mysqli_connect('localhost', 'root', 'root', 'pi');
+        if ($query_run = mysqli_query($handle, $query)) {
+            while ($query_row = mysqli_fetch_assoc($query_run)) {
+                return $query_row[$field];
+            }
+        }
+    }
+
+?>
