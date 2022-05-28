2ebf923749be2c084c2b6ca45ed480e0318e3ad2
Art Ukshini <94797611+artukshini@users.noreply.github.com>
2022-05-28 17:11:48 +0200

    Update student_submission.php

--- student_submission.php
+++ student_submission.php
@@ -1,1 +1,23 @@
+<?php
+include_once 'connect.inc.php';
+$assignmentid = $_GET['Assignmentid'];
+$subject = $_GET['Subject'];
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Student Submissions-BRIGHT MIND!</title>
+    <link rel="stylesheet" href="styles/style.css" type="text/css"/>
 
+    <link rel="stylesheet" href="styles/css1/normalize.css">
+
+
+    <link rel="stylesheet" href="styles/css1/style.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+    <style>
