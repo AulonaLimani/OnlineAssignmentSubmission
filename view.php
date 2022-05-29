7403891943a993f2ce0e6609600831a0ba58a09c
Aulona Limani <94397920+AulonaLimani@users.noreply.github.com>
2022-05-29 14:03:15 +0200

    Update view.php

--- view.php
+++ view.php
@@ -1,1 +1,7 @@
-
+<?php
+include_once 'connect.inc.php';
+include 'core.inc1.php';
+$first_name = getuserfields('Firstname');
+$last_name = getuserfields('Lastname');
+$username = getuserfields('Username');
+?>
